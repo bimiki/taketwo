58321a883452f73192f23bae640616ac1a3d1342
bimiki <noahtamulonis@gmail.com>
2021-07-05 18:53:07 -0400

    users can add to the list

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :mainthreads
+
+  has_one :profile, :dependent => :destroy
+
+after_create :myprofile
+
+def myprofile
+    profile = Profile.create!(user_id: self.id, user_name: self.name) # `.create!` will throw an error if this fails, so it's good for debugging
+    self.profile = profile
+end
+
 end
