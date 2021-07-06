53be7d176175a76823fa3bc765cc803c2404fd13
bimiki <noahtamulonis@gmail.com>
2021-07-06 10:44:40 -0400

    unstable but fixable just needs image magick to work .. xd fricking wind

---
+++ README.md
@@ -0,0 +1,6 @@
+
+to launch on windows
+imagemagick must be installed from
+https://sourceforge.net/projects/imagemagick/postdownload
+specify the version -6.9.8-4-Q16-x86-dll.exe if you need i guess its the last working version :/
+on macos, linux "sudo apt-get imagemagick -v 7.1.0"
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :mainthreads
-
-  has_one :profile, :dependent => :destroy
-
-after_create :myprofile
-
-def myprofile
-    profile = Profile.create!(user_id: self.id, user_name: self.name) # `.create!` will throw an error if this fails, so it's good for debugging
-    self.profile = profile
+  has_one_attached :avatar
+  after_commit :add_default_avatar, on: %i[create update]
+def avatar_thumbnail
+ if avatar.attached?
+  avatar.variant(resize: '150x150!').processed
+else
+  "/default_profile.jpg"
 end
-
+end
+ private
+  def add_default_avatar
+    unless avatar.attached?
+      avatar.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'default_profile.jpg'
+          )
+        ),
+         filename: 'default_profile.jpg',
+        content_type: 'image/jpg'
+      )
+    end
+  end
 end
