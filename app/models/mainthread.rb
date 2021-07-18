568f14416e2169d84a38bcd21e47f9ae324878cf
noah <noahtamulonis@gmail.com>
2021-07-18 12:15:55 -0400

    uugh ok to much to do rn ill look at it later

--- app/models/mainthread.rb
+++ app/models/mainthread.rb
@@ -1,8 +1,7 @@
 class Mainthread < ApplicationRecord
-  has_many :users
 
-  validates :name, presence: true
   after_create_commit { broadcast_prepend_to "mainthreads" }
   after_update_commit { broadcast_replace_to "mainthreads" }
   after_destroy_commit { broadcast_remove_to "mainthreads" }
+
 end
